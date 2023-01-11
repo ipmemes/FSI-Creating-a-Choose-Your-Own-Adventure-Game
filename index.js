21e31566d1756d84dd5df0be94cc704ae74aa637
Rotseni <accesslife.inc@gmail.com>
2023-01-10 22:52:56 -0800

    javaPractice

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 //Your Code here
 
-let response = window.prompt('Question: Do you head left or right?'); window is not defined
+let response = window.prompt('Question: Are you satisfied with the Software Development Intro Class: yes or no?'); 
 console.log(response.toLowerCase());
-if (response === 'right'){
-    console.log('RESPONSE HOLDS RIGHT');
-
-    let response2 = window.prompt('Go up or Down');
-
-    if(response2 === 'up'){
-
-    } else if (response2 === 'down'){
-
-    } else
-} else if (response === 'left') {
-    console.log('RESPONSE HOLDS LEFT');
-} else {
-    console.log('YOU DONT KNOW HOW TO FOLLOW INSTRUCTUIONS!!!');
+if (response === 'yes'){
+    console.log('RESPONSE HOLDS YES');
+        let response2 = window.prompt('Glad to hear that. Are interested or not interested in the extended class?');
+} else if (response === 'no') {
+    console.log('RESPONSE HOLDS NO')
+        let response2 = window.prompt('Sorry to hear that, please tell us what you dislike about the class');
 }
+
+    if (response2 === 'interested'){
+        console.log('RESPONSE HOLDS INTERESTED');
+            let response3 = window.prompt('How are you going to fund for the extended class: cash or loan?');
+    }
+
+        if(response3 === 'cash'){
+            console.log('RESPONSE HOLDS CASH');
+        }
+        let response4 = window.prompt('Great! See you on January 31st. Please enter your full name to be added to our call list before the class starts');
+  
+    
